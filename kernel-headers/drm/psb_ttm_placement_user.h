850c32d911ae7e6c6c0dc55ca2517b0dfc470534
sayeed99 <sayeed.afridi2009@gmail.com>
2017-06-11 22:59:47 +0530

    [DNM] Z00A : Update ttm header for dmabuf
    Change-Id: I7ded2d21a1bb300865d98e16e7d27e301b539f98

--- kernel-headers/drm/psb_ttm_placement_user.h
+++ kernel-headers/drm/psb_ttm_placement_user.h
@@ -37,12 +37,20 @@ struct ttm_pl_create_req {
  uint32_t page_alignment;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 };
-struct ttm_pl_create_ub_req {
- uint64_t size;
- uint64_t user_address;
+struct ttm_pl_create_userptr_req {
+	uint64_t size;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- uint32_t placement;
- uint32_t page_alignment;
+	uint64_t user_address;
+	uint32_t placement;
+	uint32_t page_alignment;
+};
+struct ttm_pl_create_dmabuf_req {
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+	uint64_t size;
+	int32_t  dmabuf_fd;
+	uint32_t placement;
+	uint32_t page_alignment;
+	uint32_t pad64;
 };
 struct ttm_pl_rep {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
@@ -107,9 +115,15 @@ union ttm_pl_setstatus_arg {
  struct ttm_pl_rep rep;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 };
-union ttm_pl_create_ub_arg {
- struct ttm_pl_create_ub_req req;
- struct ttm_pl_rep rep;
+union ttm_pl_create_userptr_arg {
+	struct ttm_pl_create_userptr_req req;
+	struct ttm_pl_rep rep;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+};
+
+union ttm_pl_create_dmabuf_arg {
+	struct ttm_pl_create_dmabuf_req req;
+	struct ttm_pl_rep rep;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 };
 #define TTM_PL_CREATE 0x00
@@ -119,6 +133,7 @@ union ttm_pl_create_ub_arg {
 #define TTM_PL_SYNCCPU 0x03
 #define TTM_PL_WAITIDLE 0x04
 #define TTM_PL_SETSTATUS 0x05
-#define TTM_PL_CREATE_UB 0x06
+#define TTM_PL_CREATE_USERPTR  0x06
+#define TTM_PL_CREATE_DMABUF   0x07
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #endif
