378666aaa92b2fe50b6a2d3ee46d21fcd2081b64
jrior001 <jriordan001@gmail.com>
2017-09-01 00:01:46 -0400

    Z00A: this is lineage!
    Change-Id: Ibec707426640e22ddd2ff29c115abad4ab7dcba5

--- lineage.mk
+++ lineage.mk
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00A/device.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/Z00A/overlay
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-add_lunch_combo cm_Z00A-userdebug
-add_lunch_combo cm_Z00A-eng
+add_lunch_combo lineage_Z00A-userdebug
+add_lunch_combo lineage_Z00A-eng
